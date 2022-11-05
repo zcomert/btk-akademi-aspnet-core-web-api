e468b1ebf22e2811292185630b29d53ddbf850ac
zcomert <zafer.comert@hotmail.com>
2022-11-05 17:55:01 +0300

    IServiceManager has been defined.

---
+++ bsStoreApp/Services/Contracts/IServiceManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Contracts
+{
+    public interface IServiceManager
+    {
+        IBookService BookService { get; }
+    }
+}
