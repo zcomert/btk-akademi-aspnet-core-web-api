732a0789a6606e2f6ff3da60618efdbbef729af9
zcomert <zafer.comert@hotmail.com>
2022-11-05 16:30:10 +0300

    RepositoryBase has been created.

---
+++ bsStoreApp/Repositories/EFCore/RepositoryBase.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.EFCore
+{
+    public abstract class RepositoryBase<T> : IRepositoryBase<T>
+        where T : class
+    {
+        protected readonly RepositoryContext _context;
+
+        public RepositoryBase(RepositoryContext context)
+        {
+            _context = context;
+        }
+
+        public void Create(T entity) => _context.Set<T>().Add(entity);
+        
+        public void Delete(T entity) => _context.Set<T>().Remove(entity);
+
+        public IQueryable<T> FindAll(bool trackChanges) =>
+            !trackChanges ?
+            _context.Set<T>().AsNoTracking() :
+            _context.Set<T>();
+
+        public IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression,
+            bool trackChanges) =>
+            !trackChanges ?
+            _context.Set<T>().Where(expression).AsNoTracking() :
+            _context.Set<T>().Where(expression);
+
+        public void Update(T entity) => _context.Set<T>().Update(entity);   
+       
+    }
+}
