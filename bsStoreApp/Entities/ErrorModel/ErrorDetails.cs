69a7f5228abb3f19d96f0053990bb888066c44b1
zcomert <zafer.comert@hotmail.com>
2022-11-06 18:51:21 +0300

    ErrorDetails has been defined.

---
+++ bsStoreApp/Entities/ErrorModel/ErrorDetails.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.ErrorModel
+{
+    public class ErrorDetails
+    {
+        public int StatusCode { get; set; }
+        public string? Message { get; set; }
+    }
+}
