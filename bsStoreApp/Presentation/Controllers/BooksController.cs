15cddd7479b2d3b1bef145bce1df3ecb1451fc45
zcomert <zafer.comert@hotmail.com>
2022-11-06 19:53:21 +0300

    global error handling has been implemented.

--- bsStoreApp/Presentation/Controllers/BooksController.cs
+++ bsStoreApp/Presentation/Controllers/BooksController.cs
@@ -1,4 +1,5 @@
-﻿using Entities.Models;
+﻿using Entities.Exceptions;
+using Entities.Models;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using Services.Contracts;
@@ -23,87 +24,47 @@ namespace Presentation.Controllers
         [HttpGet]
         public IActionResult GetAllBooks()
         {
-            try
-            {
-                var books = _manager.BookService.GetAllBooks(false);
-                return Ok(books);
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
+            var books = _manager.BookService.GetAllBooks(false);
+            return Ok(books);
         }
 
         [HttpGet("{id:int}")]
         public IActionResult GetOneBook([FromRoute(Name = "id")] int id)
         {
-            try
-            {
-                throw new Exception("!!!!");
-                var book = _manager
-                .BookService
-                .GetOneBookById(id, false);
-
-                if (book is null)
-                    return NotFound(); //404
-
-                return Ok(book);
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
-
+            var book = _manager
+            .BookService
+            .GetOneBookById(id, false);
+            
+            return Ok(book);
         }
 
         [HttpPost]
         public IActionResult CreateOneBook([FromBody] Book book)
         {
-            try
-            {
-                if (book is null)
-                    return BadRequest(); // 400 
+            if (book is null)
+                return BadRequest(); // 400 
 
-                _manager.BookService.CreateOneBook(book);
+            _manager.BookService.CreateOneBook(book);
 
-                return StatusCode(201, book);
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(ex.Message);
-            }
+            return StatusCode(201, book);
         }
 
         [HttpPut("{id:int}")]
         public IActionResult UpdateOneBook([FromRoute(Name = "id")] int id,
             [FromBody] Book book)
         {
-            try
-            {
-                if (book is null)
-                    return BadRequest(); // 400 
+            if (book is null)
+                return BadRequest(); // 400 
 
-                _manager.BookService.UpdateOneBook(id, book, true);
-                return NoContent(); // 204
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
+            _manager.BookService.UpdateOneBook(id, book, true);
+            return NoContent(); // 204
         }
 
         [HttpDelete("{id:int}")]
         public IActionResult DeleteOneBook([FromRoute(Name = "id")] int id)
         {
-            try
-            {
-                _manager.BookService.DeleteOneBook(id, false);
-                return NoContent();
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
+            _manager.BookService.DeleteOneBook(id, false);
+            return NoContent();
         }
 
 
@@ -111,25 +72,15 @@ namespace Presentation.Controllers
         public IActionResult PartiallyUpdateOneBook([FromRoute(Name = "id")] int id,
             [FromBody] JsonPatchDocument<Book> bookPatch)
         {
-            try
-            {
-                // check entity
-                var entity = _manager
-                    .BookService
-                    .GetOneBookById(id, true);
+            // check entity
+            var entity = _manager
+                .BookService
+                .GetOneBookById(id, true);
 
-                if (entity is null)
-                    return NotFound(); // 404
+            bookPatch.ApplyTo(entity);
+            _manager.BookService.UpdateOneBook(id, entity, true);
 
-                bookPatch.ApplyTo(entity);
-                _manager.BookService.UpdateOneBook(id, entity, true);
-
-                return NoContent(); // 204
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
+            return NoContent(); // 204
         }
     }
 }
--- bsStoreApp/WebApi/Extensions/ExceptionMiddlewareExtensions.cs
+++ bsStoreApp/WebApi/Extensions/ExceptionMiddlewareExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Entities.ErrorModel;
+using Entities.Exceptions;
 using Microsoft.AspNetCore.Diagnostics;
 using Services.Contracts;
 using System.Net;
@@ -14,16 +15,21 @@ namespace WebApi.Extensions
             {
                 appError.Run(async context =>
                 {
-                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
                     context.Response.ContentType = "application/json";
-
                     var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
                     if(contextFeature is not null)
                     {
+                        context.Response.StatusCode = contextFeature.Error switch
+                        {
+                            NotFoundException => StatusCodes.Status404NotFound,
+                            _ => StatusCodes.Status500InternalServerError
+                        };
+
                         logger.LogError($"Something went wrong: {contextFeature.Error}");
+                        
                         await context.Response.WriteAsync(new ErrorDetails() { 
                             StatusCode = context.Response.StatusCode,
-                            Message = "Internal Server Error"
+                            Message = contextFeature.Error.Message
                         }.ToString());
                     }
                 });
