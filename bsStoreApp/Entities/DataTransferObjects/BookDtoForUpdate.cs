b5fdc28f13eb21c0c785304a2e0841965560f082
zcomert <zafer.comert@hotmail.com>
2022-11-06 22:47:43 +0300

    Mapping profile has been created.

---
+++ bsStoreApp/Entities/DataTransferObjects/BookDtoForUpdate.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.DataTransferObjects
+{
+    public record BookDtoForUpdate(int Id, String Title, decimal Price);
+}
---
+++ bsStoreApp/WebApi/Utilities/AutoMapper/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Entities.DataTransferObjects;
+using Entities.Models;
+
+namespace WebApi.Utilities.AutoMapper
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<BookDtoForUpdate, Book>();
+        }
+    }
+}
