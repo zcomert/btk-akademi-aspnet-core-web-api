bef2a3a17bc5abde1eb25eb5fbc65fdb6734042d
zcomert <zafer.comert@hotmail.com>
2022-11-06 15:36:08 +0300

    LoggerManager has been defined.

---
+++ bsStoreApp/Services/LoggerManager.cs
@@ -0,0 +1,30 @@
+﻿using NLog;
+using Services.Contracts;
+
+
+namespace Services
+{
+    public class LoggerManager : ILoggerService
+    {
+        private static ILogger logger;
+        public void LogDebug(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogError(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogInfo(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogWarning(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
