48adb1183097f4bb1dc4af4d8710ce8ed83260d1
zcomert <zafer.comert@hotmail.com>
2022-11-08 10:47:02 +0300

    Record types have been defined.

---
+++ bsStoreApp/Entities/DataTransferObjects/BookDtoForInsertion.cs
@@ -0,0 +1,7 @@
+﻿namespace Entities.DataTransferObjects
+{
+    public record BookDtoForInsertion : BookDtoForManipulation
+    {
+
+    }
+}
--- bsStoreApp/Entities/DataTransferObjects/BookDtoForUpdate.cs
+++ bsStoreApp/Entities/DataTransferObjects/BookDtoForUpdate.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Entities.DataTransferObjects
 {
-    public record BookDtoForUpdate(int Id, String Title, decimal Price);
+    public record BookDtoForUpdate : BookDtoForManipulation
+    {
+        [Required]
+        public int Id { get; set; }
+    }
 }
