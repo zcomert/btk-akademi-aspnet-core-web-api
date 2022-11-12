4cbb2ecf74d74a2f26b01a2b2342b50e989a79f8
zcomert <zafer.comert@hotmail.com>
2022-11-12 16:19:59 +0300

    Sorting has been implemented.

--- bsStoreApp/Repositories/EFCore/Extensions/BookRepositoryExtensions.cs
+++ bsStoreApp/Repositories/EFCore/Extensions/BookRepositoryExtensions.cs
@@ -32,34 +32,8 @@ namespace Repositories.EFCore.Extensions
             if (string.IsNullOrWhiteSpace(orderByQueryString))
                 return books.OrderBy(b => b.Id);
 
-            var orderParams = orderByQueryString.Trim().Split(',');
-
-            var propertyInfos = typeof(Book)
-                .GetProperties(BindingFlags.Public | BindingFlags.Instance);
-
-            var orderQueryBuilder = new StringBuilder();
-
-            // title asceding, price descending, id ascending[,]
-            foreach (var param in orderParams)
-            {
-                if (string.IsNullOrWhiteSpace(param))
-                    continue;
-
-                var propertyFromQueryName = param.Split(' ')[0];
-
-                var objectProperty = propertyInfos
-                    .FirstOrDefault(pi => pi.Name.Equals(propertyFromQueryName,
-                    StringComparison.InvariantCultureIgnoreCase));
-
-                if (objectProperty is null)
-                    continue;
-
-                var direction = param.EndsWith(" desc") ? "descending" : "ascending";
-
-                orderQueryBuilder.Append($"{objectProperty.Name.ToString()}  {direction},");
-            }
-
-            var orderQuery = orderQueryBuilder.ToString().TrimEnd(',', ' ');
+            var orderQuery = OrderQueryBuilder
+                .CreateOrderQuery<Book>(orderByQueryString);
 
             if (orderQuery is null)
                 return books.OrderBy(b => b.Id);
