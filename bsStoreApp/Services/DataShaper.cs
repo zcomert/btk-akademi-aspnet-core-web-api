08d63d162366d0cbb2d7ffa580866f03cc8b5c3a
zcomert <zafer.comert@hotmail.com>
2022-11-12 18:07:02 +0300

    DataShaper is ongoing...

---
+++ bsStoreApp/Services/DataShaper.cs
@@ -0,0 +1,58 @@
+﻿using Services.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class DataShaper<T> : IDataShaper<T>
+        where T : class
+    {
+        public PropertyInfo[] Properties { get; set; }
+        public DataShaper()
+        {
+            Properties = typeof(T)
+                .GetProperties(BindingFlags.Public | BindingFlags.Instance);
+        }
+        public IEnumerable<ExpandoObject> ShapeData(IEnumerable<T> entities, string fieldsString)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ExpandoObject ShapeData(T entity, string fieldsString)
+        {
+            throw new NotImplementedException();
+        }
+
+        private IEnumerable<PropertyInfo> GetRequiredProperties(string fieldsString)
+        {
+            var requiredFields = new List<PropertyInfo>();
+            if(!string.IsNullOrWhiteSpace(fieldsString))
+            {
+                var fields = fieldsString.Split(',', 
+                    StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (var field in fields)
+                {
+                    var property = Properties
+                        .FirstOrDefault(pi => pi.Name.Equals(field.Trim(),
+                        StringComparison.InvariantCultureIgnoreCase));
+                    if (property is null)
+                        continue;
+                }
+            }
+            else
+            {
+                requiredFields = Properties.ToList();
+            }
+
+            return requiredFields;
+        }
+
+        
+    }
+}
