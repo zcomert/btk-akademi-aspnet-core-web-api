287229a24bcf72cd5b2aac027fb111ae6e0567e9
zcomert <zafer.comert@hotmail.com>
2022-11-12 15:12:10 +0300

    QueryString has been defined.

--- bsStoreApp/Entities/RequestFeatures/BookParameters.cs
+++ bsStoreApp/Entities/RequestFeatures/BookParameters.cs
@@ -7,5 +7,10 @@
 		public bool ValidPriceRange => MaxPrice > MinPrice;
 
 		public String? SearchTerm { get; set; }
+
+		public BookParameters()
+		{
+			OrderBy = "id";
+		}
 	}
 }
--- bsStoreApp/Entities/RequestFeatures/RequestParameters.cs
+++ bsStoreApp/Entities/RequestFeatures/RequestParameters.cs
@@ -22,5 +22,7 @@ namespace Entities.RequestFeatures
 			set { _pageSize = value > maxPageSize ? maxPageSize : value; }
 		}
 
+		public String? OrderBy { get; set; }
+
 	}
 }
