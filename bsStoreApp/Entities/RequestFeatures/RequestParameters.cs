2744bc1d30d8869a4239ae1344cf3eddadfb532e
zcomert <zafer.comert@hotmail.com>
2022-11-12 17:47:56 +0300

    IDataShapper has been defined.

--- bsStoreApp/Entities/RequestFeatures/RequestParameters.cs
+++ bsStoreApp/Entities/RequestFeatures/RequestParameters.cs
@@ -23,6 +23,7 @@ namespace Entities.RequestFeatures
 		}
 
 		public String? OrderBy { get; set; }
+		public String? Fields { get; set; }
 
 	}
 }
---
+++ bsStoreApp/Services/Contracts/IDataShaper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Contracts
+{
+    public interface IDataShaper<T>
+    {
+        IEnumerable<ExpandoObject> ShapeData(IEnumerable<T> entities, string fieldsString);
+        ExpandoObject ShapeData(T entity, string fieldsString);
+    }
+}
