71ed6da578c548deecfdc2ba75a778ff9857b3bf
zcomert <zafer.comert@hotmail.com>
2022-11-13 18:39:07 +0300

    LinkParameters has been defined.

--- bsStoreApp/WebApi/Program.cs
+++ bsStoreApp/WebApi/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using NLog;
 using Presentation.ActionFilters;
 using Repositories.EFCore;
+using Services;
 using Services.Contracts;
 using WebApi.Extensions;
 
@@ -38,6 +39,7 @@ builder.Services.ConfigureActionFilters();
 builder.Services.ConfigureCors();
 builder.Services.ConfigureDataShaper();
 builder.Services.AddCustomMediaTypes();
+builder.Services.AddScoped<IBookLinks, BookLinks>();
 
 var app = builder.Build();
 
