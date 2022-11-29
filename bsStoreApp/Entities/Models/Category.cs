7e25c17db8af33bf7a1f6c85e2a3730f3a16ba5d
zcomert <zafer.comert@hotmail.com>
2022-11-29 11:55:02 +0300

    Category has been defined.

---
+++ bsStoreApp/Entities/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public String? CategoryName { get; set; }
+    }
+}
