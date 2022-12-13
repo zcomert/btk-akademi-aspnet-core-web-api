095a8c200e5ee7c7a4065d27faaf135a4bb7f9ca
zcomert <zafer.comert@hotmail.com>
2022-12-13 13:15:28 +0300

    Updated.

--- hello_world/Program.cs
+++
@@ -1,9 +0,0 @@
-var builder = WebApplication.CreateBuilder(args);
-
-var app = builder.Build();
-
-app.Run( async (context) => {
-    await context.Response.WriteAsync("Hello Web API.");
-});
-
-app.Run();
