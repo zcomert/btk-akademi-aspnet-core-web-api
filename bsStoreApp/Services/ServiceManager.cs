f63eafbd7f6d221cf07e1a29c397533ddb9cc066
zcomert <zafer.comert@hotmail.com>
2022-12-21 14:50:19 +0300

    IoC registration has been updated.

--- bsStoreApp/Services/ServiceManager.cs
+++ bsStoreApp/Services/ServiceManager.cs
@@ -10,34 +10,23 @@ namespace Services
 {
     public class ServiceManager : IServiceManager
     {
-        private readonly Lazy<IBookService> _bookService;
-        private readonly Lazy<ICategoryService> _categoryService;
-        private readonly Lazy<IAuthenticationService> _authencationService;
-        
-        public ServiceManager(IRepositoryManager repositoryManager,
-            ILoggerService logger,
-            IMapper mapper,
-            IConfiguration configuration,
-            UserManager<User> userManager,
-            IBookLinks bookLinks)
+        private readonly IBookService _bookService;
+        private readonly ICategoryService _categoryService;
+        private readonly IAuthenticationService _authencationService;
+
+        public ServiceManager(IBookService bookService, 
+            ICategoryService categoryService, 
+            IAuthenticationService authencationService)
         {
-
-            _categoryService = new Lazy<ICategoryService>(() =>
-            new CategoryManager(repositoryManager));
-
-            _bookService = new Lazy<IBookService>(() => 
-            new BookManager(repositoryManager, logger, mapper, bookLinks, _categoryService.Value));
-
-            _authencationService = new Lazy<IAuthenticationService>(() => 
-            new AuthenticationManager(logger, mapper,userManager, configuration));
+            _bookService = bookService;
+            _categoryService = categoryService;
+            _authencationService = authencationService;
         }
-        public IBookService BookService => 
-            _bookService.Value;
 
-        public IAuthenticationService AuthenticationService => 
-            _authencationService.Value;
+        public IBookService BookService => _bookService;
 
-        public ICategoryService CategoryService => 
-            _categoryService.Value;
+        public IAuthenticationService AuthenticationService => _authencationService;
+
+        public ICategoryService CategoryService => _categoryService;
     }
 }
