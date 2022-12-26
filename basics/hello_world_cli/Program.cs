473f17ec4c20236c6141877c2ce82bacae37d586
zcomert <zafer.comert@hotmail.com>
2022-12-26 07:44:51 +0300

    Projects have been updated.

--- basics/hello_world_cli/Program.cs
+++ basics/hello_world_cli/Program.cs
@@ -1,7 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers()
-    .AddNewtonsoftJson();
+builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
